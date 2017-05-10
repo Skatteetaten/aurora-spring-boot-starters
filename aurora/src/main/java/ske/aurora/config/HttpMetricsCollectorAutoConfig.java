c94541b44fb0249df78b8367278385b334627454
Bjarte Stien Karlsen <Bjarte.Karlsen@skatteetaten.no>
2017-05-10 23:02:42 +0200

    extracted prometheus to its own library.

---
+++ aurora/src/main/java/ske/aurora/config/HttpMetricsCollectorAutoConfig.java
@@ -0,0 +1,37 @@
+package ske.aurora.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import ske.aurora.prometheus.HttpMetricsCollectorConfig;
+import ske.aurora.prometheus.collector.HttpMetricsCollector;
+
+@Configuration
+@EnableConfigurationProperties({ HttpMetricsCollectorAutoConfig.AuroraClientConfiguration.class,
+    HttpMetricsCollectorAutoConfig.AuroraServerConfiguration.class})
+public class HttpMetricsCollectorAutoConfig {
+
+    @Bean(name = "client")
+    public HttpMetricsCollector clientHttpMetricsCollector(AuroraClientConfiguration configuration) {
+        return new HttpMetricsCollector(true, configuration);
+    }
+
+    @Bean(name = "server")
+    public HttpMetricsCollector serverHttpMetricsCollector(AuroraServerConfiguration configuration) {
+        return new HttpMetricsCollector(false, configuration);
+    }
+
+
+    @ConfigurationProperties(prefix = "aurora.server")
+    static class AuroraServerConfiguration extends HttpMetricsCollectorConfig {
+
+    }
+
+    @ConfigurationProperties(prefix = "aurora.client")
+    static class AuroraClientConfiguration extends HttpMetricsCollectorConfig {
+
+    }
+
+}
---
+++ aurora/src/main/java/ske/aurora/config/MetricsAutoConfig.java
@@ -0,0 +1,23 @@
+package ske.aurora.config;
+
+import java.util.Set;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.prometheus.client.CollectorRegistry;
+import ske.aurora.prometheus.MetricsConfig;
+import ske.aurora.prometheus.collector.HttpMetricsCollector;
+
+@Configuration
+public class MetricsAutoConfig {
+
+    @Bean
+    public CollectorRegistry prometheusRegistry(Set<HttpMetricsCollector> httpCollectors) {
+
+        return MetricsConfig.init(httpCollectors);
+
+    }
+
+}
