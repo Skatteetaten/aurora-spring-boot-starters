c8e0a220013f8edd04ea5210d673fb3a3df14cf6
Bjarte Stien Karlsen <Bjarte.Karlsen@skatteetaten.no>
2017-05-10 23:13:20 +0200

    bump aurora-prometheus

--- aurora/src/main/java/ske/aurora/config/MetricsAutoConfig.java
+++ aurora/src/main/java/ske/aurora/config/MetricsAutoConfig.java
@@ -2,7 +2,6 @@ package ske.aurora.config;
 
 import java.util.Set;
 
-import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
