47d74f4fe91f4049551a0ecd2949a4ad60db175e
Bjarte Stien Karlsen <Bjarte.Karlsen@skatteetaten.no>
2017-05-16 15:20:53 +0200

    added a gracefull shutdown handler. Requests already in-flight will be given 10 seconds to finish.

---
+++ aurora/src/main/java/ske/aurora/GracefulShutdown.java
@@ -0,0 +1,44 @@
+package ske.aurora;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.catalina.connector.Connector;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextClosedEvent;
+
+public class GracefulShutdown implements TomcatConnectorCustomizer, ApplicationListener<ContextClosedEvent> {
+
+    private static Logger logger = LoggerFactory.getLogger(GracefulShutdown.class);
+    private Connector connector;
+
+    @Override
+    public void customize(Connector connector) {
+        this.connector = connector;
+    }
+
+    @Override
+    public void onApplicationEvent(ContextClosedEvent contextClosedEvent) {
+        this.connector.pause();
+        Executor executor = this.connector.getProtocolHandler().getExecutor();
+        if (!(executor instanceof ThreadPoolExecutor)) {
+            return;
+        }
+
+        ThreadPoolExecutor tpe = (ThreadPoolExecutor) executor;
+        try {
+            tpe.shutdown();
+            if (!tpe.awaitTermination(10, TimeUnit.SECONDS)) {
+                logger.warn(
+                    "Tomcat thread pool did not shut down gracefully within $shutdownTimeout $unit. Proceeding with "
+                        + "forceful shutdown");
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+}
